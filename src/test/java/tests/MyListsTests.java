70f15ac7db5a006dfd00bc2b8d43c63e3bc3796a
Andrey Popov <anrepov@gmail.com>
2020-11-12 17:16:09 +0300

    Ex11

--- src/test/java/tests/MyListsTests.java
+++ src/test/java/tests/MyListsTests.java
@@ -1,10 +1,15 @@
 package tests;
 
 import lib.CoreTestCase;
+import lib.Platform;
 import lib.ui.ArticlePageObject;
 import lib.ui.MyListsPageObject;
 import lib.ui.NavigationPageObject;
 import lib.ui.SearchPageObject;
+import lib.ui.factories.ArticlePageObjectFactory;
+import lib.ui.factories.MyListsPageObjectFactory;
+import lib.ui.factories.NavigationPageObjectFactory;
+import lib.ui.factories.SearchPageObjectFactory;
 import org.junit.Test;
 
 import java.util.List;
@@ -13,27 +18,46 @@ public class MyListsTests extends CoreTestCase {
 
     @Test
     public void testSaveTwoArticlesToListThenDeleteOne() {
-        SearchPageObject searchPageObject = new SearchPageObject(driver);
-        ArticlePageObject articlePageObject = new ArticlePageObject(driver);
-        MyListsPageObject myListsPageObject = new MyListsPageObject(driver);
-        NavigationPageObject navigationPageObject = new NavigationPageObject(driver);
+        SearchPageObject searchPageObject = SearchPageObjectFactory.get(driver);
+        ArticlePageObject articlePageObject = ArticlePageObjectFactory.get(driver);
+        MyListsPageObject myListsPageObject = MyListsPageObjectFactory.get(driver);
+        NavigationPageObject navigationPageObject = NavigationPageObjectFactory.get(driver);
 
         String articleTitle;
         String listName = "testList";
         searchPageObject.searchText("Java");
 
-        articleTitle = "Java (programming language)";
+        if (Platform.getInstance().isAndroid()) articleTitle = "Java (programming language)";
+        else articleTitle = "programming language";
         searchPageObject.selectResultWithText(articleTitle);
-        articlePageObject.addToList(articleTitle, listName);
 
-        driver.navigate().back();
+        if (Platform.getInstance().isAndroid()) {
+            articlePageObject.addToList(articleTitle, listName);
+            driver.navigate().back();
+        } else {
+            articlePageObject.addArticlesToMySaved();
+            navigationPageObject.goToMainPage();
+            searchPageObject.searchText("Java");
+        }
 
-        articleTitle = "Java";
+        if (Platform.getInstance().isAndroid()) articleTitle = "Java";
+        else articleTitle = "Island of Indonesia";
         searchPageObject.selectResultWithText(articleTitle);
-        articlePageObject.addToList(articleTitle, listName);
+
+        if (Platform.getInstance().isAndroid()) {
+            articlePageObject.addToList(articleTitle, listName);
+            driver.navigate().back();
+        } else {
+            articlePageObject.addArticlesToMySaved();
+        }
 
         navigationPageObject.goToMainPage();
-        myListsPageObject.openSavedList(listName);
+
+        if (Platform.getInstance().isAndroid())
+            myListsPageObject.openSavedList(listName);
+        else
+            navigationPageObject.goToMyListsFromMainPage();
+
         List<String> articlesBeforeDelete = myListsPageObject.getCurrentArticlesNames();
 
         int deletedArticleIndex = myListsPageObject.deleteAnyArticle();
