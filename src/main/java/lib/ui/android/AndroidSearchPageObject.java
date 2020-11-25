b1436acd3fddbf2c498f4c1202270f2bdeaeadb9
anrepov <anrepov@gmail.com>
2020-11-25 09:48:46 +0300

    update

--- src/main/java/lib/ui/android/AndroidSearchPageObject.java
+++ src/main/java/lib/ui/android/AndroidSearchPageObject.java
@@ -10,15 +10,15 @@ public class AndroidSearchPageObject extends SearchPageObject {
         SEARCH_LINE = "id:org.wikipedia:id/search_src_text";
         SEARCH_RESULTS = "xpath://*[@resource-id = 'org.wikipedia:id/search_results_list']/*[@class='android.view.ViewGroup']";
         SEARCH_CANCEL_BUTTON = "id:org.wikipedia:id/search_close_btn";
-        SEARCH_RESULT_BY_INDEX_TPL = "//*[@resource-id = 'org.wikipedia:id/search_results_list']" +
+        SEARCH_RESULT_BY_INDEX_TPL = "xpath://*[@resource-id = 'org.wikipedia:id/search_results_list']" +
                 "/*[@class='android.view.ViewGroup']" +
                 "[@index = '%s']" +
                 "/*[@resource-id = 'org.wikipedia:id/page_list_item_title']";
-        SEARCH_RESULT_BY_TEXT_TPL = "//*[@resource-id = 'org.wikipedia:id/search_results_list']" +
+        SEARCH_RESULT_BY_TEXT_TPL = "xpath://*[@resource-id = 'org.wikipedia:id/search_results_list']" +
                 "/*[@class='android.view.ViewGroup']" +
                 "/*[@resource-id = 'org.wikipedia:id/page_list_item_title']" +
                 "[@text = '%s']";
-        SEARCH_RESULT_BY_TEXT_AND_DESCRIPTION_TPL = "//*[@resource-id = 'org.wikipedia:id/search_results_list']" +
+        SEARCH_RESULT_BY_TEXT_AND_DESCRIPTION_TPL = "xpath://*[@resource-id = 'org.wikipedia:id/search_results_list']" +
                 "//*[@resource-id = 'org.wikipedia:id/page_list_item_title' and ../../*[@class='android.view.ViewGroup'] and @text = '%s' " +
                 "and following-sibling::*[@resource-id = 'org.wikipedia:id/page_list_item_description' and @text = '%s']]";
     }
